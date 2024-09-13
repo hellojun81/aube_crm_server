2712e03b662c9eab1227920c0b792431dd3a995c
김완준 <>
2024-09-13 19:29:59 +0900

    t

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ import columnsRoutes from './routes/columns.js';
 import customersRoutes from './routes/customers.js';
 import scheduleRoutes from './routes/schedules.js';
 import setupRoutes from './routes/setup.js';
-
+import http from 'http';
 //nodemon --exec ./node_modules/.bin/ts-node web.js
 dotenv.config();
-
 const app = express();
+const httpServer = http.createServer(app);
 const port = process.env.PORT || 8001;
 
 app.use(express.json());
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.header("Access-Control-Allow-Credentials", 'true')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // 모든 HTTP 메서드 허용
     res.header('Content-Type', "application/json")
-    res.json('welcome')
+    res.json('welcome 123')
 })
 
 app.use('/api/columns', columnsRoutes);
@@ -32,6 +32,6 @@ app.use('/api/customers', customersRoutes);
 app.use('/api/schedules', scheduleRoutes);
 app.use('/api/setup', setupRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+httpServer.listen(8001, function (req, res) {
+    console.log('server start')
+})
