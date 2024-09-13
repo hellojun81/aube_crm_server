bb3afa68abbbdff09fff23dca87d8d1e26cc572d
김완준 <>
2024-09-13 16:44:47 +0900

    t

--- web.js
+++ web.js
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.header("Access-Control-Allow-Credentials", 'true')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // 모든 HTTP 메서드 허용
     res.header('Content-Type', "application/json")
-    res.json('welcome 123')
+    res.json('welcome start 123')
 })
 
 app.use('/api/columns', columnsRoutes);
